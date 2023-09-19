f982ca12b620f7f8b618fd02fff07d0636c3ef75
a-shdv <shadaev2001@icloud.com>
2023-09-19 21:07:59 +0400

    Refactor code

--- main.go
+++ main.go
@@ -4,26 +4,21 @@ import (
 	"apache-kafka-golang/kafka"
 	"apache-kafka-golang/model"
 	"context"
-	"encoding/json"
 	"github.com/joho/godotenv"
 	kafkago "github.com/segmentio/kafka-go"
 	"golang.org/x/sync/errgroup"
-	"io"
 	"log"
-	"net/http"
-	"time"
 )
 
-var client *http.Client
-var person model.Person
 var urls []string
 
 func main() {
-	client = &http.Client{Timeout: 5 * time.Second}
+	var helper model.Helper
+
 	urls = []string{
 		"https://api.agify.io/?name=Dmitriy",
 		"https://api.genderize.io/?name=Dmitriy",
-		"https://api.nationalize.io/?name=Dmitriy", // TODO stucture creation from field country
+		"https://api.nationalize.io/?name=Dmitriy",
 	}
 
 	// Загрузка переменных окружения
@@ -37,31 +32,35 @@ func main() {
 
 	// Инициализация каналов
 	ctx := context.Background()
-	messages := make(chan kafkago.Message, 1e3)
-	messageCommitChan := make(chan kafkago.Message, 1e6)
+	kafkaFromChanMessages := make(chan kafkago.Message, 1e3)
+	kafkaToChanMessages := make(chan kafkago.Message, 1e3)
 
 	//  Инициализация группы горутин 'g' соответствующим контекстом 'ctx'
 	g, ctx := errgroup.WithContext(ctx)
 
 	// Получение сообщений с канала
 	g.Go(func() error {
-		return reader.FetchMessage(ctx, messages)
+		return reader.FetchMessage(ctx, kafkaFromChanMessages)
 	})
 
-	// Person
-	getJsonFromKafka(messages)
-	getJsonFromUrls(urls)
+	person := model.Person{}
+	helper.ParseAndStoreJsonData(urls, &person)
+	helper.StoreKafkaMessages(kafkaFromChanMessages, &person)
 
-	log.Printf("person: %+v\n", person)
+	log.Printf("person: %s\n", person.Name)
+	log.Printf("person: %s\n", person.Surname)
+	log.Printf("person: %s\n", person.Patronymic)
+	log.Printf("person: %d\n", person.Age)
+	log.Printf("person: %s\n", person.Gender)
 
 	// Запись сообщений в другой канал
 	g.Go(func() error {
-		return writer.WriteMessages(ctx, messages, messageCommitChan)
+		return writer.WriteMessages(ctx, kafkaFromChanMessages, kafkaToChanMessages)
 	})
 
 	// Фиксация сообщений - в противном случае сообщения отправятся в другой канал еще раз
 	g.Go(func() error {
-		return reader.CommitMessages(ctx, messageCommitChan)
+		return reader.CommitMessages(ctx, kafkaToChanMessages)
 	})
 
 	// Блокирующая операция
@@ -69,45 +68,4 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-}
-
-func getJsonFromKafka(messages chan kafkago.Message) {
-	for msg := range messages {
-		var err error
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = json.Unmarshal(msg.Value, &person)
-		if err != nil {
-			log.Fatalf("error while unmarshal: %s\n", err.Error())
-		}
-
-		break
-	}
-}
-
-func getJsonFromUrls(urls []string) {
-	for _, url := range urls {
-		err := decodeJsonToStruct(url, &person)
-		if err != nil {
-			log.Fatalf("error while decoding json: %s\n", err.Error())
-		}
-	}
-}
-
-func decodeJsonToStruct(url string, target interface{}) error {
-	resp, err := client.Get(url)
-	if err != nil {
-		log.Printf("could not parse json froum url: %s\n", err.Error())
-		return err
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			log.Fatalf("err: ", err.Error())
-		}
-	}(resp.Body)
-	return json.NewDecoder(resp.Body).Decode(target)
 }
---
+++ model/helper.go
@@ -0,0 +1,56 @@
+package model
+
+import (
+	"encoding/json"
+	kafkago "github.com/segmentio/kafka-go"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Helper struct {
+	Client http.Client
+}
+
+// parseJsonData Обрабатывает json-данные, полученные из URL-строки
+func (h *Helper) parseJsonData(input string) (*http.Response, error) {
+	resp, err := h.Client.Get(input)
+	if err != nil {
+		log.Printf("could not get data from url: %s\n", err.Error())
+		return nil, err
+	}
+	return resp, nil
+}
+
+// storeJsonData Заполняет структуру json-данными, получеными из URL-строки
+func storeJsonData(resp *http.Response, target interface{}) error {
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatalf("error while closing the connection: ", err.Error())
+		}
+	}(resp.Body)
+
+	return json.NewDecoder(resp.Body).Decode(target)
+}
+
+// ParseAndStoreJsonData Заполняет структуру данными из массива URL-строк
+func (h *Helper) ParseAndStoreJsonData(urls []string, target interface{}) {
+	for _, url := range urls {
+		resp, err := h.parseJsonData(url)
+		if err != nil {
+			log.Fatalf("could not parse json-data from url: %s\n", err.Error())
+		}
+		err = storeJsonData(resp, &target)
+	}
+}
+
+// StoreKafkaMessages Заполняет структуру данными из очередей kafka
+func (h *Helper) StoreKafkaMessages(kafkaMessages chan kafkago.Message, target interface{}) {
+	for msg := range kafkaMessages {
+		err := json.Unmarshal(msg.Value, &target)
+		if err != nil {
+			log.Fatalf("could not fill target with message value: %s\n", err.Error())
+		}
+	}
+}
